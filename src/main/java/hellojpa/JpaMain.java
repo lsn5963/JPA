3189efc906598a4bd3b9d4cf7b165fe7c0019470
lsn5963 <lsn5963@naver.com>
2022-10-12 22:21:47 +0900

    Refactor: 강의 진행

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -11,6 +11,40 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try{
+//            Team team = new Team();
+//            team.setName("TeamA");
+//            em.persist(team);
+//
+//            Member member = new Member();
+//            member.setUsername("member1");
+//            member.setTeamId(team.getId());
+//            em.persist(member);
+//
+//            Member findMember = em.find(Member.class, member.getId());
+//            Long findTeamId = findMember.getTeamId();
+//
+//            Team findTeam = em.find(Team.class, findTeamId);    //연관관계가 없어서 이게 너무 귀찮고 매핑관계가 없기 때문에 객체지향스럽지 않음
+            // 저장
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeam(team);
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+            List<Member> members = findMember.getTeam().getMembers();
+
+            for (Member m : members) {
+                System.out.println("m" + m.getUsername() );
+            }
+
+
             tx.commit();
         }catch (Exception e){
             tx.rollback();
