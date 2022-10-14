4efae6c798d94f5b69182e0ac69c02e372b31f40
이상남 <lsn5963@naver.com>
2022-10-14 15:54:40 +0900

    REFACTOR: 리펙토링

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -5,7 +5,10 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
 @Entity
 @Getter
 public class Member {
@@ -20,6 +23,12 @@ public class Member {
     @JoinColumn(name = "TEAM_ID")
     private Team team;
 
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProduct = new ArrayList<>();
     public void setId(Long id) {
         this.id = id;
     }
